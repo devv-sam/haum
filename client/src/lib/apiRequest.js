412fbab75daa97323ec4e374cb542a269ea17912
devsam <samuelydev@gmail.com>
2025-01-27 14:12:57 -0500

    error: 405 destructuring render api link

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: "https://haum.onrender.com",
   withCredentials: true,
 });
 
