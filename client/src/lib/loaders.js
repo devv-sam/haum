e1c8849677d97713d1ba1f324942f5948e9d6b74
devsam <samuelydev@gmail.com>
2025-01-27 14:29:22 -0500

    working to resolve error loading posts

--- client/src/lib/loaders.js
+++ client/src/lib/loaders.js
@@ -1,19 +1,19 @@
 import apiRequest from "../lib/apiRequest";
 import { defer } from "react-router-dom";
 const singlePageLoader = async ({ request, params }) => {
-  const res = await apiRequest("/posts/" + params.id);
+  const res = await apiRequest("/api/posts/" + params.id);
   return res.data;
 };
 
 const listPageLoader = async ({ request, params }) => {
   const query = request.url.split("?")[1];
-  const postPromise = apiRequest("/posts?" + query);
+  const postPromise = apiRequest("/api/posts?" + query);
   return defer({
     postResponse: postPromise,
   });
 };
 const profilePageLoader = async () => {
-  const postPromise = apiRequest("/users/profilePosts");
+  const postPromise = apiRequest("/api/users/profilePosts");
   return defer({
     postResponse: postPromise,
   });
